1a61d0819268248d3cd44201bca256ee089b3996
Ahmed Amin <Ahmedaminhassanismail@gmail.com>
2021-06-27 18:11:31 +0200

    create users table view and custom cell and add fake data

---
+++ MVP-With-Router/Users/UsersCell/UsersViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  UsersViewCell.swift
+//  MVP-With-Router
+//
+//  Created by Amin  on 6/27/21.
+//  Copyright © 2021 AhmedAmin. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewCell: UITableViewCell {
+    
+    // outlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        selectionStyle = .none
+    }
+    
+}
